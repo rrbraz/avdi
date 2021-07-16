bd87ca62eb00d536e9406fe373da1703f24df57a
Rafael Braz <rafael@rbraz.net>
2021-07-15 22:11:10 -0300

    add grommet and login page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import { Grommet } from 'grommet';
+import '../styles/globals.css';
+
+const theme = {
+  "global": {
+    "colors": {
+      "background": {
+        "light": "#ffffff",
+        "dark": "#000000"
+      }
+    },
+    "font": {
+      "family": "-apple-system,\n         BlinkMacSystemFont, \n         \"Segoe UI\", \n         Roboto, \n         Oxygen, \n         Ubuntu, \n         Cantarell, \n         \"Fira Sans\", \n         \"Droid Sans\",  \n         \"Helvetica Neue\", \n         Arial, sans-serif,  \n         \"Apple Color Emoji\", \n         \"Segoe UI Emoji\", \n         \"Segoe UI Symbol\""
+    }
+  },
+  "button": {
+    "extend": [
+      null
+    ]
+  }
+}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Grommet full theme={theme}>
+      <Component {...pageProps} />
+    </Grommet>
+  );
 }
 
 export default MyApp
